7d5de8a5d0a4ce6b3ea974bf2a618cc25400a303
MohamedAliAmean <mdiab0109666@gmail.com>
2025-06-30 13:15:11 +0300

    delete unused Registration component

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import LandingPage from "./components/LandingPage/LandingPage.jsx";
 import Register2 from "./components/Register2/Register.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<Register2 />} />
       </Routes>
       <Footer />
-    </Router>
+    </>
   );
 }
 
