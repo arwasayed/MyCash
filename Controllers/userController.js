d36c4f410f797c29cf91451510ecba7f96b7682a
mariam sameh shafeek <mariamsamehzx12@gmail.com>
2025-07-12 02:33:51 +0300

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const User = require('../models/userModel');
 const sendEmail = require('../utils/email');
-const passport = require('passport');
+// const passport = require('passport');
 const OAuth2Client = require ('google-auth-library');
 
 const sendResponse = (res, statusCode, status, data, message) => {
