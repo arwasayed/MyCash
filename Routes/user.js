e4a34da3ef2f67d53938f153977782d188d00ddc
mariam sameh shafeek <mariamsamehzx12@gmail.com>
2025-07-12 02:31:57 +0300

    Update user.js

--- Routes/user.js
+++ Routes/user.js
@@ -16,22 +16,24 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
 router.patch('/reset-password/:token', resetPassword);
-router.get('/verify-email/:token', verifyEmail); 
+router.get('/verify-email/:token', verifyEmail);
 
-//google
-router.get('/auth/google',
-  passport.authenticate('google', {
-    scope: ['email', 'profile'],
-    session: false,
-    prompt: 'select_account' 
-  })
-);
-router.get('/auth/google/callback',
-  passport.authenticate('google', {
-    failureRedirect: '/login',
-    session: false
-  }),
-  googleCallback 
-);
+router.post('/google', googleAuth);
+
+// //google
+// router.get('/auth/google',
+//   passport.authenticate('google', {
+//     scope: ['email', 'profile'],
+//     session: false,
+//     prompt: 'select_account' 
+//   })
+// );
+// router.get('/auth/google/callback',
+//   passport.authenticate('google', {
+//     failureRedirect: '/login',
+//     session: false
+//   }),
+//   googleCallback 
+// );
 
 module.exports = router;
