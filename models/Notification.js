3f8c430fe309049bf4fc78d307f6e29dd94b7961
mariam sameh shafeek <mariamsamehzx12@gmail.com>
2025-07-13 20:36:20 +0300

    Update Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userId: { type: String, required: true }, 
   message: { type: String, required: true },
   type: {
     type: String,
