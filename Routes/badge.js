b23e9e71b8c5048c2e0f47cbf08ef7960ed9d83e
Nada-Salah-Abizeid <nada.salah.it@gmail.com>
2025-07-16 18:18:58 +0300

    AI generated challenges

--- Routes/badge.js
+++ Routes/badge.js
@@ -4,7 +4,7 @@ const { protect, restrictTo } = require('../middlewares/authMiddleware');
 const {getBadges, getUserBadges, deleteBadge, createBadge, updateBadge } = require('../Controllers/badge');
 
 
-router.get('/', protect, getBadges);
+router.get('/', protect, restrictTo('admin'), getBadges);
 router.get('/user', protect, getUserBadges);
 router.post('/', protect, restrictTo('admin'), createBadge);
 router.delete('/:id', protect, restrictTo('admin'), deleteBadge);
--- Routes/challenge.js
+++ Routes/challenge.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { joinChallenge, completeChallenge, getActiveChallenges, createChallenge, getAllChallenges, updateChallenge, deleteChallenge } = require('../Controllers/challenge');
+const { joinChallenge, completeChallenge, getUserChallenges, createChallenge, getAllChallenges, updateChallenge, deleteChallenge } = require('../Controllers/challenge');
 const { protect, restrictTo } = require('../middlewares/authMiddleware');
 
-router.get("/", protect, getAllChallenges);
+router.get("/", protect, restrictTo('admin'), getAllChallenges);
 router.put("/:id", protect,restrictTo('admin'), updateChallenge);
 router.delete("/:id", protect, restrictTo('admin'), deleteChallenge);
 router.post('/join/:id', protect, joinChallenge);
 router.post('/complete/:id', protect, completeChallenge);
-router.get('/active', protect, getActiveChallenges);
+router.get('/active', protect, getUserChallenges);
 router.post('/', protect, restrictTo('admin'), createChallenge);
+
 
 module.exports = router;
